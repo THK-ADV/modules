7765860cab36ecf77c6229780897c06ae8f56c9f
Alexander Dobrynin <info@alex-dobrynin.com>
2025-05-19 17:39:49 +0200

    improved error page. adds uuid param validation

---
+++ src/params/uuid.ts
@@ -0,0 +1,6 @@
+import { isUUID } from "$lib/utils";
+import type { ParamMatcher } from "@sveltejs/kit";
+
+export const match: ParamMatcher = (param) => {
+  return isUUID(param)
+}
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
 	import { page } from '$app/state'
+
+	// throw Error() is used for unexpected errors. Its error message is always 'Internal Error', since it does not expose sensitive informations to the users
+	const isUnexpectedError = page.error?.message === 'Internal Error'
+	const title = isUnexpectedError
+		? `${page.status} - Ein unerwarteter Fehler ist aufgetreten`
+		: `${page.status} - Ein Fehler ist aufgetreten`
+	const message = isUnexpectedError ? undefined : page.error?.message
 </script>
 
-<pre>{page.status}: {JSON.stringify(page, null, 2)}</pre>
+<div class="space-y-2">
+	<h1 class="text-2xl font-bold tracking-tight">{title}</h1>
+	{#if message}
+		<p>Fehlernachricht: {message}</p>
+	{/if}
+	<div class="h-px w-full bg-border"></div>
+	<pre
+		class="overflow-x-auto rounded-md border border-border bg-muted p-4 font-mono text-sm text-muted-foreground">{JSON.stringify(
+			page,
+			null,
+			2
+		)}</pre>
+</div>

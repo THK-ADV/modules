1363ddbf7f6baf11a0c743a64aed6980c4955860
Alexander Dobrynin <info@alex-dobrynin.com>
2025-05-20 22:04:55 +0200

    lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,27 @@
 module.exports = {
-  root: true,
+	root: true,
+	ignorePatterns: ['node_modules/', '.svelte-kit/', 'build/', 'dist/'],
 
-  // Use the TypeScript parser and allow .svelte files
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    extraFileExtensions: ['.svelte']
-  },
+	// Use the Svelte ESLint parser for everything
+	parser: 'svelte-eslint-parser',
+	parserOptions: {
+		// This is the "inner" parser for <script lang="ts"> blocks
+		parser: '@typescript-eslint/parser',
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		extraFileExtensions: ['.svelte']
+	},
 
-  plugins: [
-    '@typescript-eslint',
-    'svelte'
-  ],
+	plugins: ['svelte', '@typescript-eslint'],
 
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended'
-  ],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended'
+	],
 
-  rules: {
-    // turn off the no-at-html-tags rule globally
-    'svelte/no-at-html-tags': 'off'
-  },
-
-  ignorePatterns: [
-    'node_modules/',
-    '.svelte-kit/',
-    'build/',
-    'dist/'
-  ]
+	rules: {
+		// globally turn off the no-at-html-tags rule
+		'svelte/no-at-html-tags': 'off'
+	}
 }

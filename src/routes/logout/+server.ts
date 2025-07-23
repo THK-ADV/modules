bef7ff384fdb1f15a657c26e50091bfc496aa35d
Alexander Dobrynin <info@alex-dobrynin.com>
2025-07-23 16:02:59 +0200

    adds proper keycloak logout flow

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -1,9 +1,10 @@
-import { deleteCookies } from '$lib/server/auth'
+import { performLogout } from '$lib/server/auth'
 import { redirect } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 
-// This is a poor mans version of a logout, since the redirect to the app does not work properly
-export const GET: RequestHandler = async ({ cookies }) => {
-  deleteCookies(cookies)
-  throw redirect(303, '/')
+export const GET: RequestHandler = async ({ cookies, fetch, url }) => {
+  const baseUrl = `${url.protocol}//${url.host}`
+  const redirectTo = url.searchParams.get('redirectTo') || '/'
+  const logoutUrl = await performLogout(cookies, fetch, baseUrl, redirectTo)
+  throw redirect(303, logoutUrl)
 }

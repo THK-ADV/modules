77046c5e7f4b9583ef350f5ff6f5def5d1311f25
Alexander Dobrynin <info@alex-dobrynin.com>
2025-08-06 13:34:05 +0200

    feat: add module type filter and inactive indicator to module search (#19)

--- src/routes/modules/+page.ts
+++ src/routes/modules/+page.ts
@@ -1,7 +1,33 @@
 import { moduleFilter } from '$lib/store.svelte'
-import type { ModuleView } from '$lib/types/module'
+import type {
+  ModuleType,
+  ModuleView,
+  ModuleViewJson,
+  StudyProgramModuleAssociation
+} from '$lib/types/module'
 import { error } from '@sveltejs/kit'
 import type { PageLoad } from './$types'
+
+function getModuleType(studyPrograms: StudyProgramModuleAssociation[]): ModuleType | undefined {
+  let mandatory = false
+  let elective = false
+  for (const s of studyPrograms) {
+    if (s.mandatory) {
+      mandatory = true
+    } else {
+      elective = true
+    }
+  }
+  if (mandatory && !elective) {
+    return { id: 'pm', label: 'Pflichtmodul', abbrev: 'PM' }
+  } else if (!mandatory && elective) {
+    return { id: 'wm', label: 'Wahlmodul', abbrev: 'WM' }
+  } else if (mandatory && elective) {
+    return { id: 'pwm', label: 'Als Modul wählbar', abbrev: 'P/WM' }
+  } else {
+    return undefined
+  }
+}
 
 export const load: PageLoad = async ({ fetch, params }) => {
   await moduleFilter.init(fetch)
@@ -9,11 +35,10 @@ export const load: PageLoad = async ({ fetch, params }) => {
   if (!res.ok) {
     throw error(res.status, `Failed to load ${params}`)
   }
-  const modules: ModuleView[] = await res.json()
-  for (const m of modules) {
-    m.studyProgram.sort((a, b) =>
-      a.studyProgram.abbreviation.localeCompare(b.studyProgram.abbreviation)
-    )
-  }
+  const json: ModuleViewJson[] = await res.json()
+  const modules: ModuleView[] = json.map((m) => ({
+    ...m,
+    moduleType: getModuleType(m.studyProgram)
+  }))
   return { modules }
 }
